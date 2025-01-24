1b30c9826900b453398ee4ffb8408a2ecde6f2df
vaibhavxom <rmithagare02@gmail.com>
2025-01-24 21:25:37 +0530

    Initial commit

---
+++ accounts/forms.py
@@ -0,0 +1,125 @@
+from django import forms
+from django.conf import settings
+from django.contrib.auth.forms import UserCreationForm
+from django.db import transaction
+
+from .models import User, BankAccountType, UserBankAccount, UserAddress
+from .constants import GENDER_CHOICE
+
+
+class UserAddressForm(forms.ModelForm):
+    class Meta:
+        model = UserAddress
+        fields = [
+            'street_address',
+            'city',
+            'postal_code',
+            'country',
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': (
+                    'appearance-none block w-full bg-gray-200 '
+                    'text-gray-700 border border-gray-200 rounded '
+                    'py-3 px-4 leading-tight focus:outline-none '
+                    'focus:bg-white focus:border-gray-500'
+                ),
+                'placeholder': field.replace('_', ' ').capitalize()
+            })
+
+
+class UserRegistrationForm(UserCreationForm):
+    account_type = forms.ModelChoiceField(
+        queryset=BankAccountType.objects.all(),
+        widget=forms.Select(attrs={
+            'class': (
+                'appearance-none block w-full bg-gray-200 '
+                'text-gray-700 border border-gray-200 '
+                'rounded py-3 px-4 leading-tight '
+                'focus:outline-none focus:bg-white '
+                'focus:border-gray-500'
+            ),
+        })
+    )
+    gender = forms.ChoiceField(
+        choices=GENDER_CHOICE,
+        widget=forms.Select(attrs={
+            'class': (
+                'appearance-none block w-full bg-gray-200 '
+                'text-gray-700 border border-gray-200 '
+                'rounded py-3 px-4 leading-tight '
+                'focus:outline-none focus:bg-white '
+                'focus:border-gray-500'
+            ),
+        })
+    )
+    birth_date = forms.DateField(
+        widget=forms.DateInput(attrs={
+            'class': (
+                'appearance-none block w-full bg-gray-200 '
+                'text-gray-700 border border-gray-200 '
+                'rounded py-3 px-4 leading-tight '
+                'focus:outline-none focus:bg-white '
+                'focus:border-gray-500'
+            ),
+            'placeholder': 'YYYY-MM-DD',
+            'type': 'date',  # Ensure proper date input rendering
+        })
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'email',
+            'password1',
+            'password2',
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email Address',
+            'password1': 'Enter Password',
+            'password2': 'Confirm Password',
+        }
+
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': (
+                    'appearance-none block w-full bg-gray-200 '
+                    'text-gray-700 border border-gray-200 '
+                    'rounded py-3 px-4 leading-tight '
+                    'focus:outline-none focus:bg-white '
+                    'focus:border-gray-500'
+                ),
+                'placeholder': placeholders.get(field, field.replace('_', ' ').capitalize())
+            })
+
+    @transaction.atomic
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+            account_type = self.cleaned_data.get('account_type')
+            gender = self.cleaned_data.get('gender')
+            birth_date = self.cleaned_data.get('birth_date')
+
+            UserBankAccount.objects.create(
+                user=user,
+                gender=gender,
+                birth_date=birth_date,
+                account_type=account_type,
+                account_no=(
+                    user.id +
+                    settings.ACCOUNT_NUMBER_START_FROM
+                )
+            )
+        return user
